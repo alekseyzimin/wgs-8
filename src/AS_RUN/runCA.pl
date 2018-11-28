e24958b95624b46b1b09a1828f927182c36d066f
Aleksey Zimin <alekseyz@ipst.umd.edu>
2018-11-28 10:00:17 -0500

    SLURM manual submit support for CA

--- src/AS_RUN/runCA.pl
+++ src/AS_RUN/runCA.pl
@@ -87,7 +87,7 @@ sub submitBatchJobs($$) {
         runCommand($wrk, getGlobal("gridSubmitCommand").$SGE."\n") and caFailure("Failed to submit batch jobs.");
         submitScript($TAG);
     } else {
-        if(getGlobal("gridSubmitCommand") eq "slurm"){
+        if(getGlobal("gridEngine") eq "SLURM"){
         pleaseExecute(getGlobal("gridSubmitCommand").$SGE."\n");
         exit(0);
         }
